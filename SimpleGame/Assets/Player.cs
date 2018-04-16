822d0311e3875d7593e5299113b65910d3c20c0c
Junbin Kwon (Jun) <wnsqls901@gmail.com>
2018-04-16 18:54:36 -0400

    Finished character animation

---
+++ SimpleGame/Assets/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody2D rigid;
+	SpriteRenderer render;
+	Animator animator;
+	public Vector2 moveSpeed = new Vector2(150, 150);
+
+	private Vector2 movement;
+	// Use this for initialization
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody2D> ();
+		render = gameObject.GetComponentInChildren<SpriteRenderer> ();
+		animator = gameObject.GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		Move ();
+	}
+
+	void Move() {
+		float inputX = Input.GetAxisRaw ("Horizontal");
+		float inputY = Input.GetAxisRaw ("Vertical");
+
+		if (inputX == 0 && inputY == 0) {
+			animator.SetBool ("isMoving", false);
+		}
+		if (inputX < 0) {
+			render.flipX = true;
+			animator.SetInteger ("Direction", -1);
+			animator.SetBool ("isMoving", true);
+		} else if (inputX > 0) {
+			render.flipX = false;
+			animator.SetInteger ("Direction", 1);
+			animator.SetBool ("isMoving", true);
+		}
+		if (inputY != 0) {
+			animator.SetInteger ("Direction", 1);
+			animator.SetBool ("isMoving", true);
+		}
+
+		movement = new Vector2 (
+			moveSpeed.x * inputX * Time.deltaTime,
+			moveSpeed.y * inputY * Time.deltaTime
+		);
+
+		rigid.velocity = movement;
+	}
+}
